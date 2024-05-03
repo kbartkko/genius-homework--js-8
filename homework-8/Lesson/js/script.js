7306bef737ecafdc8c415eadfb0b839e1a5fce85
kbartkko <146842563+kbartkko@users.noreply.github.com>
2024-05-03 14:18:44 +0200

    Add files via upload

---
+++ homework-8/Lesson/js/script.js
@@ -0,0 +1,85 @@
+// let elem = document.getElementById('elem');
+
+// elem.style.background = "green";
+
+// let elements = document.querySelectorAll('ul>li:last-child')
+
+// for (element of elements) {
+//     console.log(element);
+// }
+
+// let textHidden = document.getElementById("textHidden")
+// textHidden.hidden = true;
+
+// let message = document.getElementById('message');
+// console.log(message.value);
+
+// let text = document.querySelector(".text-message");
+// console.log(text.textContent);
+
+// let text = document.querySelector(".text-message");
+// text.textContent = "Hello, Kate!"
+
+// let btn = document.querySelector(".button");
+// btn.style.backgroundColor = 'red';
+
+
+// let text = document.querySelector('#p_text');
+
+// console.log(text.classList.contains('red'));
+
+// text.classList.remove('red')
+// console.log((text.classList));
+// console.log((text.classList.contains('red')));
+
+
+// let image = document.querySelector(".image");
+// console.log(image.getAttribute("src"));
+// console.log(image.hasAttribute("src"));
+
+
+// const item = document.createElement("a");
+// item.href = "#";
+// item.classList.add("btn");
+// item.textContent = "3";
+
+// const nav = document.querySelector(".nav");
+// nav.appendChild(item);
+
+// const heading = document.createElement('h1');
+// heading.textContent = "Hello";
+
+// const container = document.querySelector(".container")
+
+// container.insertBefore(heading, nav);
+
+// nav.removeChild(item);
+
+// item.remove();
+
+
+
+// const text = document.querySelector(".text");
+// const parent = document.querySelector(".parent");
+
+// const clone = text.cloneNode(true);
+
+// parent.appendChild(clone);
+
+
+// const container = document.querySelector(".container")
+
+// const text = `<p>Sorry</p>`;
+
+// container.innerHTML += text;
+
+
+// const list = document.querySelector("#list");
+
+// list.insertAdjacentHTML('beforebegin', '<p>beforebegin</p>');
+// list.insertAdjacentHTML('afterbegin', '<li>afterbegin</li>');
+// list.insertAdjacentHTML('beforeend', '<li>beforeend</li>');
+// list.insertAdjacentHTML('afterend', '<p>afterend</p>');
+
+
+    
